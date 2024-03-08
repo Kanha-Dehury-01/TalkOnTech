45dea3ff929ec06cc772946313dacc0632336c34
Kanha Dehury <kanhadehury2001@gmail.com>
2024-03-08 17:49:38 +0530

    text color update

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -12,7 +12,7 @@ const Input = React.forwardRef(function Input(
             )}
             <input
                 type={type}
-                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+                className={`px-3 py-2 rounded-lg bg-white dark:text-white font-semibold outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
                 id={id}
                 ref = {ref}
                 {...props}
